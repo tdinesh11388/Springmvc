6c8f9341e8c4d911fe87eff0f662c340ef1fcae9
admin <admin@DESKTOP-9UKH062>
2023-05-30 22:09:33 +0530

    commite first

---
+++ VisterDataManagement/src/main/java/com/spring/visiterdatamanagement/base/VisterDataManagementApplication.java
@@ -0,0 +1,13 @@
+package com.spring.visiterdatamanagement.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisterDataManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisterDataManagementApplication.class, args);
+	}
+
+}
---
+++ VisterDataManagement/src/test/java/com/spring/visiterdatamanagement/base/VisterDataManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.visiterdatamanagement.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisterDataManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
